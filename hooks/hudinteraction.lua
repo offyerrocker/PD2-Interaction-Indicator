63a59922ec9fb5887d459df08a24bafe95b0606b
offyerrocker <offyerrocker88@gmail.com>
2023-08-23 17:38:14 -0700

    Changed instanced hudinteraction class
    Avoid crashes or unintended behavior caused by teammates/ai using the same class for interaction timers: clone base HUDInteraction class, create separate instance for local interactions (as opposed to teammate interactions), hook that class instead
    Does not currently work with custom HUDs; HUD compatibility will need be re-addressed in a later update

--- hooks/hudinteraction.lua
+++ hooks/hudinteraction.lua
@@ -1,44 +1,50 @@
-Hooks:PostHook(HUDInteraction,"init","interactionindicator_init",function(self,hud,child_name)
+
+HUDInteractionIndicator = class(HUDInteraction)
+
+--posthooking a custom class... galaxy brained
+local hooked_class = HUDInteractionIndicator
+
+Hooks:PostHook(hooked_class,"init","interactionindicator_init",function(self,hud,child_name)
 	InteractionIndicator:CreateHUD()
 end)
 
-Hooks:PostHook(HUDInteraction,"show_interact","interactionindicator_showinteract",function(self,data)
+Hooks:PostHook(hooked_class,"show_interact","interactionindicator_showinteract",function(self,data)
 --	OffyLib:c_log("show_interact")
 	InteractionIndicator:OnStartMouseoverInteractable(self,data)
 	--InteractionIndicator:ShowInteractText(self,data)
 end)
 
-Hooks:PostHook(HUDInteraction,"remove_interact","interactionindicator_removeinteract",function(self)
+Hooks:PostHook(hooked_class,"remove_interact","interactionindicator_removeinteract",function(self)
 --	OffyLib:c_log("remove_interact")
 	InteractionIndicator:OnStopInteraction(self)
 	--InteractionIndicator:HideInteractText(self)
 end)
 
-Hooks:PostHook(HUDInteraction,"show_interaction_bar","interactionindicator_showinteractionbar",function(self,current,total)
+Hooks:PostHook(hooked_class,"show_interaction_bar","interactionindicator_showinteractionbar",function(self,current,total)
 --	OffyLib:c_log("show_interaction_bar " .. string.format("%0.1f",current) .. "/" .. string.format("%0.1f",total))
 	InteractionIndicator:OnInteractionStart(self,current,total)
 --	InteractionIndicator:ShowInteractionBar(self,current,total)
 end)
 
-Hooks:PostHook(HUDInteraction,"set_interaction_bar_width","interactionindicator_setinteractionprogress",function(self,current,total)
+Hooks:PostHook(hooked_class,"set_interaction_bar_width","interactionindicator_setinteractionprogress",function(self,current,total)
 --	OffyLib:c_log("set_interaction_bar_width " .. string.format("%0.1f",current) .. "/" .. string.format("%0.1f",total))
 	InteractionIndicator:SetInteractionProgress(self,current,total)
 	--InteractionIndicator:ShowInteractionProgress(self,current,total)
 end)
 
-Hooks:PostHook(HUDInteraction,"hide_interaction_bar","interactionindicator_hideinteractionbar",function(self,complete)
+Hooks:PostHook(hooked_class,"hide_interaction_bar","interactionindicator_hideinteractionbar",function(self,complete)
 --	OffyLib:c_log("hide_interaction_bar " .. tostring(complete))
 	InteractionIndicator:OnInteractionEnd(self,complete)
 	--InteractionIndicator:HideInteractionBar(self,complete)
 end)
 
-Hooks:PostHook(HUDInteraction,"set_bar_valid","interactionindicator_setbarvalid",function(self,valid,text_id)
+Hooks:PostHook(hooked_class,"set_bar_valid","interactionindicator_setbarvalid",function(self,valid,text_id)
 --	OffyLib:c_log("set_bar_valid")
 	InteractionIndicator:SetInteractTextValid(self,valid,text_id)
 end)
 
-local orig_animate_complete = Hooks:GetFunction(HUDInteraction,"_animate_interaction_complete")
-Hooks:OverrideFunction(HUDInteraction,"_animate_interaction_complete",function(self,bitmap,circle,...)
+local orig_animate_complete = Hooks:GetFunction(hooked_class,"_animate_interaction_complete")
+Hooks:OverrideFunction(hooked_class,"_animate_interaction_complete",function(self,bitmap,circle,...)
 	if InteractionIndicator.settings.circle_enabled then
 		if alive(bitmap) then 
 			bitmap:stop()
--- hooks/hudmanager.lua
+++ hooks/hudmanager.lua
@@ -1,4 +1,15 @@
+
+function HUDManager:_create_interaction(hud)
+	hud = hud or managers.hud:script(PlayerBase.PLAYER_INFO_HUD_PD2)
+	self._hud_interaction = HUDInteractionIndicator:new(hud)
+end
+
+
+--[[
+
 Hooks:PostHook(HUDManager,"pd_stop_progress","interactionindicator_hudmanager_stop_interact",function(self)
 	--InteractionIndicator:HideInteractionBar(nil,nil)
 	InteractionIndicator:OnInteractionEnd()
 end)
+
+--]]
